45164740c7b574eadfff752bb6f665292f02cee2
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-01 02:19:57 +0000

    write service controller

---
+++ GoodHabits.HabitService/Dtos/CreateHabitDto.cs
@@ -0,0 +1,7 @@
+namespace GoodHabits.HabitService.Dtos;
+
+public class CreateHabitDto
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
