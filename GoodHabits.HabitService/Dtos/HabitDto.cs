0aa19746571d5defc6b3c92b6558e71ad97bc3d9
Sarah Schlueter <sarahmarie73@gmail.com>
2024-05-04 20:50:14 +0000

    create HabitDto class

---
+++ GoodHabits.HabitService/Dtos/HabitDto.cs
@@ -0,0 +1,8 @@
+namespace GoodHabits.HabitService.Dtos;
+
+public class HabitDto
+{
+    public int Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
